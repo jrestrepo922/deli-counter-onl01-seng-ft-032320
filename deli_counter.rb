20d9bf4c88d29bf10d3f64bb205aa9b7ec60a641
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-25 18:44:50 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,46 @@
 # Write your code here.
+katz_deli = []
+
+# part 1
+  #customers = ["Logan", "Avi", "Spencer"]
+  
+  def line(customers)
+    new_array = []
+    if customers.size == 0 
+      puts "The line is currently empty."
+    else 
+      customers.each_with_index do |customer, index| 
+        new_array.push(" #{index + 1}. #{customer}")
+      end
+      #puts new_array.inspect
+      intermidiate_string = new_array.join
+      final_string = "The line is currently:" + intermidiate_string
+      puts final_string
+    end 
+  end 
+  
+  #puts line(customers)
+  
+#part 2 
+  #katz_deli = []
+
+  def take_a_number(line, new_customer_name)
+    line.push(new_customer_name)
+    puts "Welcome, #{line.last}. You are number #{line.index(line.last) + 1} in line."
+  end 
+  
+  #puts take_a_number(katz_deli, "Ada") 
+
+  #"Welcome, Ada. You are number 1 in line."
+  
+#part 3 
+  def now_serving(line)
+    if line.size > 0  
+      puts "Currently serving #{line.first}."
+      line.shift
+    else
+      puts "There is nobody waiting to be served!"
+    end 
+  end
+  
+  #"Currently serving Ada."
