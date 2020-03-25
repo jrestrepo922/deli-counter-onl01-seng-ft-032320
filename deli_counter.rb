f75e670b7791dab1963c53b656f1bfd869807e10
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-25 18:23:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,24 +1,29 @@
 # Write your code here.
 katz_deli = []
 
-#customers = ["Logan", "Avi", "Spencer"]
-
-def line(customers)
-  new_array = []
-  if customers.size == 0 
-    puts "The line is currently empty."
-  else 
-    customers.each_with_index do |customer, index| 
-      new_array.push(" #{index + 1}. #{customer}")
-    end
-    #puts new_array.inspect
-    intermidiate_string = new_array.join
-    final_string = "The line is currently:" + intermidiate_string
-    puts final_string
+# part 1
+  #customers = ["Logan", "Avi", "Spencer"]
+  
+  def line(customers)
+    new_array = []
+    if customers.size == 0 
+      puts "The line is currently empty."
+    else 
+      customers.each_with_index do |customer, index| 
+        new_array.push(" #{index + 1}. #{customer}")
+      end
+      #puts new_array.inspect
+      intermidiate_string = new_array.join
+      final_string = "The line is currently:" + intermidiate_string
+      puts final_string
+    end 
   end 
-end 
+  
+  #puts line(customers)
+  
+#part 2 
 
-#puts line(customers)
-
-
-
+  def take_a_number(line, new_customer_name)
+    line.push(new_customer_name)
+    
+  end 
